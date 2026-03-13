69603e52b58651b0b8457232bdcd80296298b891
angelostori <angelostori13@gmail.com>
2026-03-13 17:21:39 +0100

    implement the destroy crud function and implement the index with the delete option

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -15,6 +15,7 @@
                 <th>Telefono</th>
                 <th>Indirizzo</th>
                 <th></th>
+                <th></th>
             </tr>
         </thead>
 
@@ -27,6 +28,33 @@
                 <td class="p-3">{{ $client->phone }}</td>
                 <td class="p-3">{{ $client->address }}</td>
                 <td><a class="btn btn-outline-warning" href="{{ route('clients.edit', $client) }}">Modifica</a></td>
+                <td>
+                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal-{{ $client->id }}">
+                        <i class="bi bi-trash-fill"></i>
+                    </button>
+
+                    <div class="modal fade" id="deleteModal-{{ $client->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5">Eliminare il cliente: {{ $client->name . ' ' . $client->surname }}</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Vuoi veramente eliminare questo cliente? Questa azione è irreversibile.
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                                    <form action="{{ route('clients.destroy', $client) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Conferma e Elimina</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </td>
             </tr>
             @endforeach
         </tbody>
