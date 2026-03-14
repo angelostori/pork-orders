85a143c4198d5fce97528f506c757889d47a2a70
angelostori <angelostori13@gmail.com>
2026-03-14 11:45:06 +0100

    implement the single product show page

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.app')
 
-@section('title', $product->name)
+@section('title', 'Dettagli prodotto')
 
 @section('content')
 
-<h2>funziona</h2>
+<div class="container my-5">
+    <div class="card">
+        <div class="card-header">
+            <h3>{{ $product->name }}</h3>
+
+        </div>
+        <div class="card-body">
+            @if($product->image)
+            <div class="card-image">
+                <!-- <img src="{{ asset('storage/' . $product->image) }}" alt="copertina del progetto {{ $product->name }}" class="img-fluid pb-3"> -->
+            </div>
+            @endif
+            <h4 class="card-title">Prezzo: {{ $product->price . ' ' . '€\Kg' }}</h4>
+            <p class="card-text">{{ $product->description }}</p>
+        </div>
+        <a class="btn btn-primary" href="{{ route('products.index') }}">Torna alla lista dei prodotti</a>
+    </div>
+</div>
 
 @endsection
